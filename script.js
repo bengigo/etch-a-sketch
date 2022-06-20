33892061c7509b7cd3b3d79dc768831efc5167c8
bengigenc <genc.bengi@gmail.com>
2022-06-20 14:32:38 +0300

    Create grid elements inside container

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const grids = document.querySelector('#squares');
 
 grids.style.gridTemplateColumns = 'repeat(16, 1fr)';
 grids.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+for(let i=0; i<256; i++) {
+    let square = document.createElement('div');
+    grids.appendChild(square);
+}
