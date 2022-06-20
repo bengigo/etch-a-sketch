cdaa5bb95b8dbae8964b5e70ae1a16726e46454f
bengigenc <genc.bengi@gmail.com>
2022-06-20 17:17:06 +0300

    Set validation for maximum size

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
 function createBoard(size) {
-  const grids = document.querySelector('#squares');
-  grids.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  grids.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    const grids = document.querySelector('#squares');
+    grids.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grids.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < Math.pow(size, 2); i++) {
-    const square = document.createElement('div');
-    square.classList.toggle('square');
-    grids.appendChild(square);
-  }
+    //   check if the input is max 100. return an error message if not
+    //   if(size > 100) {
+    //     const msg = document.createElement('p');
+    //     msg.textContent = 'Maximum amount of squares per side is 100!';
+    //     const form = document.querySelector('form');
+    //     form.appendChild(msg);
+    //   }
+
+    for (let i = 0; i < Math.pow(size, 2); i++) {
+        const square = document.createElement('div');
+        square.classList.toggle('square');
+        grids.appendChild(square);
+    }
+
+    // if (size > 100) {
+    //     const msg = document.createElement('p');
+    //     msg.textContent = 'Maximum amount of squares per side is 100!';
+    //     const form = document.querySelector('form');
+    //     form.appendChild(msg);
+    // } else {
+    //     for (let i = 0; i < Math.pow(size, 2); i++) {
+    //         const square = document.createElement('div');
+    //         square.classList.toggle('square');
+    //         grids.appendChild(square);
+    //     }
+
+    // }
+
+
 }
 
 createBoard(16);
 
 const squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'gray';
-  });
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'gray';
+    });
 });
 
 squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'gray';
-  });
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'gray';
+    });
 });
-
-// function removeSquares() {
-//     const squares = document.querySelectorAll('.square');
-
-//     squares.forEach((square) => {
-//         square.remove();
-//     })
-// };
 
 const sizeButton = document.querySelector('#size');
 
 // when button is clicked propmt user for input
 sizeButton.addEventListener('click', (e) => {
-  // clear existing squares
-  // const squares = document.querySelectorAll('.square');
-  // squares.forEach((square) => {
-  //     square.remove();
-  // })
-  e.preventDefault();
-  console.log('so?');
-  createBoard(0);
+    e.preventDefault();
 
-  const size = prompt('How many squares per side do you want to set?');
-  createBoard(size);
+    createBoard(0);
+
+    const size = prompt('How many squares per side do you want to set?');
+    // createBoard(size);
+    if (size <= 100) {
+        createBoard(size);
+    } else {
+        const msg = document.createElement('p');
+        msg.textContent = 'Maximum amount of squares per side is 100!';
+        const form = document.querySelector('form');
+        form.appendChild(msg);
+
+    }
 });
