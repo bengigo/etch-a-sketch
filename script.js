4f40bb518efe400a555c7f741b7041747917f10f
bengigenc <genc.bengi@gmail.com>
2022-06-20 14:58:33 +0300

    Create pen trail effect

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const grids = document.querySelector('#squares');
 grids.style.gridTemplateColumns = 'repeat(16, 1fr)';
 grids.style.gridTemplateRows = 'repeat(16, 1fr)';
 
-for(let i=0; i<256; i++) {
-    let square = document.createElement('div');
-    grids.appendChild(square);
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.toggle('square');
+  grids.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'gray';
+    })
+})
